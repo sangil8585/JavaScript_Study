94acb6d84b8c9a9a2ee798ed4876b8e94a5cfda7
Sangil Byun <sangil8585@naver.com>
2023-12-12 08:05:12 +0900

    JS CodingTest Practice

---
+++ 03/js/quiz-1.js
@@ -0,0 +1,7 @@
+const array = [1, 3, 5, 7, 9, 11, ,13 ,15, 17, 19];
+let number = 10;
+for(let i=0 ; i <= array.length ; i++){
+    if (array[i] > 10) {
+        document.write(`${array[i]}, `);
+    }
+}
