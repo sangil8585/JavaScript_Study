7497b20001440d1601ce768b9d8e398046593547
Sangil Byun <sangil8585@naver.com>
2023-12-21 23:56:24 +0900

    순서 변경

--- 03/js/quiz-1.js
+++ 03/js/quiz-1.js
@@ -1,4 +1,4 @@
-const array = [1, 3, 5, 7, 9, 11, ,13 ,15, 17, 19];
+const array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
 let number = 10;
 for(let i=0 ; i <= array.length ; i++){
     if (array[i] > 10) {
